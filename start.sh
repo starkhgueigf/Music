609fc77cce591ae5ffbed37ed6e15deef337372a
starkhgueigf <145910732+starkhgueigf@users.noreply.github.com>
2023-09-24 20:31:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
+echo "Cloning Repo...."
+if [ -z $BRANCH ]
+then
+  echo "Cloning main branch...."
+  git clone https://github.com/starkhgueigf/Music /Music
+else
+  echo "Cloning $BRANCH branch...."
+  git clone https://github.com/starkhgueigf/Music /Music
+fi
+cd /Music
+pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
