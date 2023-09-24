2b41597599bdc7b0fa485a4a2a2a59937b4a2ede
starkhgueigf <145910732+starkhgueigf@users.noreply.github.com>
2023-09-24 20:17:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,2 @@
-echo "Cloning Repo...."
-if [ -z $BRANCH ]
-then
-  echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
-else
-  echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
-fi
-cd /VCPlayerBot
-pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
